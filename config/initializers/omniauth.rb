c1e38cb4d7784e4e418f4ef901eca48453a09fa6
hiroshi <hiroshi@hiroshi-sequoia.local>
2025-02-06 20:01:02 +0900

    Avoid errors on build.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :google_oauth2,
-    Rails.application.credentials.google_oauth2.client_id,
-    Rails.application.credentials.google_oauth2.client_secret
+    Rails.application.credentials.google_oauth2&.client_id,
+    Rails.application.credentials.google_oauth2&.client_secret
   )
 end
 
